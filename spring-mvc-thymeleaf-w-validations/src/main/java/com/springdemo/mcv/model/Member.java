117f5c60e85092b85321f99435159ae436dbdfd9
Neerajthind <152557955+Neerajthind@users.noreply.github.com>
2024-09-16 20:57:10 -0700

    Add files via upload

--- spring-mvc-thymeleaf-w-validations/src/main/java/com/springdemo/mcv/model/Member.java
+++ spring-mvc-thymeleaf-w-validations/src/main/java/com/springdemo/mcv/model/Member.java
@@ -1,6 +1,9 @@
 package com.springdemo.mcv.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class Member {
@@ -14,6 +17,15 @@ public class Member {
     @NotNull(message = "is required")
     @Size(min=1, message = "is requried")
     private String email;
+
+    @NotNull(message = "is required")
+    @Min(value=0, message = "must be greater than or equal to zero")
+    @Max(value=10, message = "must be less than or equal to 10")
+    private Integer fitnessGoal;
+
+    // using regular expression for validaiton 
+    @Pattern(regexp = "^GYM[0-9]{5}", message = "Membership ID must start with 'GYM' and exactly 5 digits") 
+    private String membershipId;
 
     public Member(){
 
@@ -41,6 +53,22 @@ public class Member {
 
     public void setEmail(String email){
         this.email = email;
+    }
+
+    public Integer getFitnessGoal(){
+        return fitnessGoal;
+    }
+
+    public void setFitnessGoal(Integer fitnessGoal){
+        this.fitnessGoal = fitnessGoal;
+    }
+
+    public String getMembershipId(){
+        return membershipId;
+    }
+
+    public void setMembershipId(String membershipId){
+        this.membershipId = membershipId;
     }
 
 }
