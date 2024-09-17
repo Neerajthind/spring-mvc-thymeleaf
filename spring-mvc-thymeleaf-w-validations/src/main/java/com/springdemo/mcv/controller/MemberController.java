3f9bef28c8f14ad512cb8ad427c5e7be1edf3255
Neerajthind <152557955+Neerajthind@users.noreply.github.com>
2024-09-16 19:55:20 -0700

    Add files via upload

--- spring-mvc-thymeleaf-w-validations/src/main/java/com/springdemo/mcv/controller/MemberController.java
+++ spring-mvc-thymeleaf-w-validations/src/main/java/com/springdemo/mcv/controller/MemberController.java
@@ -1,7 +1,10 @@
 package com.springdemo.mcv.controller;
 
+
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.springdemo.mcv.model.Member;
@@ -14,13 +17,21 @@ public class MemberController {
     @GetMapping("/")
     public String showForm(Model theModel){
         
-        theModel.addAttribute("customer", new Member());
+        theModel.addAttribute("member", new Member());
 
         return "member-form";
     }
 
     @PostMapping("/processForm")
     public String processForm(
-        
-    )
+        @Valid @ModelAttribute("member") Member theMember,
+        BindingResult theBindingResult){
+
+            if(theBindingResult.hasErrors()){
+                return "member-form";
+            }
+            else{
+                return "member-confirmation";
+            }
+        }
 }
--- spring-mvc-thymeleaf-w-validations/src/main/java/com/springdemo/mcv/model/Member.java
+++ spring-mvc-thymeleaf-w-validations/src/main/java/com/springdemo/mcv/model/Member.java
@@ -1,13 +1,18 @@
 package com.springdemo.mcv.model;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public class Member {
     
     private String firstName;
 
+    @NotNull(message = "is required")
+    @Size(min=1, message = "is requried")
     private String lastName;
 
+    @NotNull(message = "is required")
+    @Size(min=1, message = "is requried")
     private String email;
 
     public Member(){
